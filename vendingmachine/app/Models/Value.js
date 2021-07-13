5667dda1d03509e36a6ca21a6d9d25d78d6e09df
thedudeman69 <christopher.jp.newell@gmail.com>
2021-07-13 15:35:31 -0600

    working on the coin button and snack array

--- vendingmachine/app/Models/Value.js
+++ vendingmachine/app/Models/Value.js
@@ -1,13 +1,21 @@
-export default class Value {
-    constructor(data) {
-        this.title = data.title
-    }
+export default class Snacks {
+    constructor(name, price) {
+        this.names = {
 
+        name: chips {
+            price: 1.50
+        }
+        name: soda {
+            price: 2.25
+        }
+    }
+    }
+//snack somewhere here in the model?
     get Template() {
 
         return /*html*/`
         <div class="card p-2 value">
-            ${this.title}
+            ${this.coin}
         </div>
         `
     }
